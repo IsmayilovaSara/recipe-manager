f5b4a62522757e4541c09be253bff871a6853232
IsmayilovaSara <ismayilovassara@gmail.com>
2025-01-05 02:00:48 +0400

    .

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -14,7 +14,7 @@ const ContactMe = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch('http://localhost:4000/messages', {
+    fetch('http://localhost:3000/messages', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
